56529811840b71cc9307051669ffff586c89bc96
adamsheaffer <adam.e.sheaffer@gmail.com>
2015-03-16 16:46:50 -0500

    created album and user models

---
+++ TheVinylWoof/Models/Album.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheVinylWoof.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public virtual int ApplicationUserId { get; set; }
+        public virtual int BuyerId { get; set; }
+        public int Cost { get; set; }
+        public bool IsSold { get; set; }
+        public string CoverLocation { get; set; }
+    }
+}
