0d2c869e571d66e271a22ab2e17b4e9067860dd8
adamsheaffer <adam.e.sheaffer@gmail.com>
2015-03-16 20:04:48 -0500

    set api dependency resolver

--- TheVinylWoof/App_Start/NinjectWebCommon.cs
+++ TheVinylWoof/App_Start/NinjectWebCommon.cs
@@ -11,6 +11,8 @@ namespace TheVinylWoof.App_Start
     using Ninject;
     using Ninject.Web.Common;
     using TheVinylWoof.Data;
+    using System.Web.Http;
+    using WebApiContrib.IoC.Ninject;
 
     public static class NinjectWebCommon 
     {
@@ -47,6 +49,9 @@ namespace TheVinylWoof.App_Start
                 kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
 
                 RegisterServices(kernel);
+
+                GlobalConfiguration.Configuration.DependencyResolver = new NinjectResolver(kernel); 
+
                 return kernel;
             }
             catch
