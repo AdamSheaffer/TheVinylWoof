76c33d2bd77014b23a3de7072acf62ee2ebdd7c5
adamsheaffer <adam.e.sheaffer@gmail.com>
2015-03-16 21:01:17 -0500

    added main js

---
+++ TheVinylWoof/js/home-index.js
@@ -0,0 +1,40 @@
+﻿//home-index.js
+angular.module("VinylWoofApp", ["ngRoute"])
+
+.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            controller: "albumsController",
+            templateUrl: "/templates/homeView.html"
+        })
+        .when("/AddRecord", {
+            controller: "newAlbumController",
+            templateUrl: "/templates/newRecord.html"
+        })
+        .otherwise({ redirectTo: "/" });
+})
+
+.controller("albumsController", function ($scope, $http) {
+    $scope.name = "Adam Sheaffer";
+    $scope.dataCount = 0;
+    $scope.data = [];
+
+    $http.get("/api/albums")
+        .then(function (result) {
+            //success
+            console.log(result);
+            angular.copy(result.data, $scope.data);
+        },
+        function () {
+            alert("error")
+        })
+
+})
+
+.controller("newAlbumController", function ($scope, $http) {
+    $scope.newAlbum = {};
+
+    $scope.save = function () {
+        $http.post
+    }
+});
