f43240795fdbf73452674badfefe33695859a2d6
adamsheaffer <adam.e.sheaffer@gmail.com>
2015-03-19 11:45:48 -0500

    added more details styling to details page

--- TheVinylWoof/Migrations/Configuration.cs
+++ TheVinylWoof/Migrations/Configuration.cs
@@ -26,7 +26,7 @@ namespace VinylWoof.Migrations
                     Cost = 3,
                     UserId = "2d427ab5-04d1-41a4-8354-255cedc78bd8",
                     IsSold = false,
-                    Description = "Great album",
+                    Description = "Early first pressing of the Beatles penultimate album and last recording session Abbey Road. It is in good condition and sounds great on the stereo. This is a first pressing as it has the record label variant and the album cover commonly called the sewer cover. Great to have in any record collection. Email me if you have any questions",
                     CoverLocation = "http://ecx.images-amazon.com/images/I/91uC0QuxoML._SL1500_.jpg"
                 },
 
@@ -39,7 +39,7 @@ namespace VinylWoof.Migrations
                      Cost = 3,
                      UserId = "2d427ab5-04d1-41a4-8354-255cedc78bd8",
                      IsSold = false,
-                     Description = "Great album",
+                     Description = "Great album but it's time for someone else to enjoy this. Vinyl in good condition - has a couple of faint scratches on side 1.",
                      CoverLocation = "http://ecx.images-amazon.com/images/I/51481od8IuL.jpg"
                  },
 
@@ -52,7 +52,7 @@ namespace VinylWoof.Migrations
                      Cost = 3,
                      UserId = "85bddf62-9670-4aa9-8d0b-fd94ff3a271f",
                      IsSold = false,
-                     Description = "Great album",
+                     Description = "Great album but it's time for someone else to enjoy this. Vinyl in good condition - has a couple of faint scratches on side 1.",
                      CoverLocation = "http://ecx.images-amazon.com/images/I/71Ubu92jsAL._SL1500_.jpg"
                  },
 
@@ -61,12 +61,12 @@ namespace VinylWoof.Migrations
                    Title = "Locked Down",
                    Artist = "Dr. John",
                    Genre = "Blues",
-                   Condition = "Good",
+                   Condition = "Excellent",
                    Cost = 4,
                    UserId = "85bddf62-9670-4aa9-8d0b-fd94ff3a271f",
                    IsSold = false,
                    CoverLocation = "http://ecx.images-amazon.com/images/I/816Af3gozOL._SL1425_.jpg",
-                   Description = "Great album"
+                   Description = "Brand new vinyl and still sealed. Someone gave this to me and I'm just not a big Dr. John fan."
                }
             );
         }
